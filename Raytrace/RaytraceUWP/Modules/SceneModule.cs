c722a7f492d180c720f5e9545ff6ff6b64dc9319
Rino Jose <rino.jose@outlook.com>
2018-12-28 20:01:34 -0800

    Clean up scene module

--- Raytrace/RaytraceUWP/Modules/SceneModule.cs
+++ Raytrace/RaytraceUWP/Modules/SceneModule.cs
@@ -20,22 +20,20 @@ namespace RaytraceUWP
 
             this.Code = @"
             [ intersection shader canvas ] USE-MODULES
-            [ '_world' '_ray' '_intersection' '_comps' ] VARIABLES
-
-            : OBJECTS   _world @ 'objects' REC@ ;
-            : INTERSECT-WORLD   ( _ray ! _world ! ) OBJECTS  '_ray @ INTERSECTS' MAP FLATTEN  't' SORT-BY-FIELD ;
+            [ 'world' 'ray' ] VARIABLES
 
             # Words supporting PREPARE-COMPUTATIONS
          {
-            : T         _intersection @ 't' REC@ ;
-            : OBJ       _intersection @ 'obj' REC@ ;
-            : POINT     _ray @  _intersection @ 't' REC@  POSITION ;
-            : EYEV      _ray @ 'direction' REC@  NEGATE ;
+            [ 'intersection' ] VARIABLES
+            : T         intersection @ 't' REC@ ;
+            : OBJ       intersection @ 'obj' REC@ ;
+            : POINT     ray @  intersection @ 't' REC@  POSITION ;
+            : EYEV      ray @ 'direction' REC@  NEGATE ;
             : NORMALV   OBJ POINT NORMAL-AT ;
             : SIGN      NORMALV EYEV DOT SIGN ;
             : INSIDE    SIGN 0 < ;
             : NORMALV   NORMALV SIGN * ;
-            : PREPARE-COMPUTATIONS   ( _ray ! _intersection ! )
+            : PREPARE-COMPUTATIONS   ( ray ! intersection ! )
                 [  T   OBJ   POINT   EYEV   NORMALV   INSIDE ]
                 [ 't' 'obj' 'point' 'eyev' 'normalv' 'inside' ] REC
             ;
@@ -45,41 +43,48 @@ namespace RaytraceUWP
 
             # Words supporting SHADE-HIT
         {
-            [ '_comps' ] VARIABLES
-            : VAL        _comps @ SWAP REC@ ;  # ( field -- value )
+            [ 'comps' ] VARIABLES
+            : VAL        comps @ SWAP REC@ ;  # ( field -- value )
             : MATERIAL   'obj' VAL  'material' REC@ ;
-            : LIGHT      _world @ 'light' REC@ ;
+            : LIGHT      world @ 'light' REC@ ;
             : POINT      'point' VAL ;
             : EYEV       'eyev' VAL ;
             : NORMALV    'normalv' VAL ;
-            : SHADE-HIT   ( _comps ! _world ! ) MATERIAL LIGHT POINT EYEV NORMALV   LIGHTING ;
+            : SHADE-HIT   ( comps ! world ! ) MATERIAL LIGHT POINT EYEV NORMALV   LIGHTING ;
 
             [ 'SHADE-HIT' ] PUBLISH
         }
 
-            # Words supporting COLOR-AT
+            # Words supporting COLOR-AT and INTERSECT-WORLD
+        {
+            : OBJECTS   world @ 'objects' REC@ ;
+            : INTERSECT-WORLD   ( ray ! world ! ) OBJECTS  'ray @ INTERSECTS' MAP FLATTEN  't' SORT-BY-FIELD ;
+
             : COLOR-MISS   POP 0 0 0 Color ;  # ( hit -- color )
-            : COLOR-HIT   _ray @ PREPARE-COMPUTATIONS _world @ SWAP SHADE-HIT ;  # ( hit -- color )
+            : COLOR-HIT   ray @ PREPARE-COMPUTATIONS world @ SWAP SHADE-HIT ;  # ( hit -- color )
             : COLOR-AT    INTERSECT-WORLD HIT COLOR-HIT/MISS ;  # ( world ray -- color )
+
+            [ 'INTERSECT-WORLD' 'COLOR-HIT' 'COLOR-MISS' 'COLOR-AT' ] PUBLISH
+        }
 
             # Words supporting VIEW-TRANSFORM
          {
-            [ '_from' '_up' '_to' '_forward' '_-forward' '_upn' '_left' '_true_up' ] VARIABLES
+            [ 'from' 'up' 'to' 'forward' '-forward' 'upn' 'left' 'true_up' ] VARIABLES
             : X@   @ 'x' REC@ ; 
             : Y@   @ 'y' REC@ ; 
             : Z@   @ 'z' REC@ ; 
-            : FORWARD!       _to @ _from @ - NORMALIZE   _forward ! ;
-            : -FORWARD!      _forward @ NEGATE   _-forward ! ;
-            : UPN!           _up @ NORMALIZE _upn ! ;
-            : LEFT!          _forward @ _upn @  CROSS   _left ! ;
-            : TRUE-UP!       _left @ _forward @ CROSS   _true_up ! ;
+            : FORWARD!       to @ from @ - NORMALIZE   forward ! ;
+            : -FORWARD!      forward @ NEGATE   -forward ! ;
+            : UPN!           up @ NORMALIZE upn ! ;
+            : LEFT!          forward @ upn @  CROSS   left ! ;
+            : TRUE-UP!       left @ forward @ CROSS   true_up ! ;
             : INIT           FORWARD! -FORWARD! UPN! LEFT! TRUE-UP! ;
-            : ORIENTATION    [   _left X@      _left Y@      _left Z@  0
-                                _true_up X@   _true_up Y@   _true_up Z@  0
-                               _-forward X@  _-forward Y@  _-forward Z@  0
-                                          0             0             0  1 ]  Matrix ;
-            : V-TRANSLATION    [ _from X@  _from Y@ _from Z@ ] 'NEGATE' MAP UNPACK TRANSLATION ;
-            : VIEW-TRANSFORM   ( _up ! _to ! _from ! ) INIT ORIENTATION V-TRANSLATION * ;
+            : ORIENTATION    [     left X@      left Y@      left Z@  0
+                                true_up X@   true_up Y@   true_up Z@  0
+                               -forward X@  -forward Y@  -forward Z@  0
+                                         0            0            0  1 ]  Matrix ;
+            : V-TRANSLATION    [ from X@  from Y@ from Z@ ] 'NEGATE' MAP UNPACK TRANSLATION ;
+            : VIEW-TRANSFORM   ( up ! to ! from ! ) INIT ORIENTATION V-TRANSLATION * ;
 
             [ 'VIEW-TRANSFORM' ] PUBLISH
         }
